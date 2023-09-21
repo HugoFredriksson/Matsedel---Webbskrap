a74a2193a5d385de6983e1e92d248725183a0589
HugoFredriksson <hufr04001@utb.vaxjo.se>
2023-09-21 08:57:57 +0200

    Påbörjat, lade till en enkel webbskrapa som kan skrapa enstaka värden.

---
+++ webskrap/webskrap/Program.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+using System.Text.Encodings.Web;
+
+public class WebScraper
+{
+
+    public static async Task Main(string[] args)
+    {
+        var url = "https://mpi.mashie.com/public/app/V%C3%A4xj%C3%B6%20kommun%20ny/6f5fa240";
+        var httpClient = new HttpClient();
+        var html = await httpClient.GetStringAsync(url);
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+        var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("app-daymenu-name")).ToList();
+        foreach ( var div in divs )
+        {
+            Console.WriteLine(div.InnerText);
+            Console.WriteLine();
+        }
+
+    }
+}
